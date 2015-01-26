fe70b3f5092e094e97e34b8ef8e419c82e237d7b
Nguyen Ngo <nqngo@ucdavis.edu>
2015-01-25 18:24:37 -0800

    need map

--- app/src/main/java/nu/gumi/demo/util/MapFragment.java
+++ app/src/main/java/nu/gumi/demo/util/MapFragment.java
@@ -1,5 +1,8 @@
 package nu.gumi.demo.util;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -23,7 +26,25 @@ public class MapFragment extends Fragment{
         View v = inflater.inflate(R.layout.fragment_map, parent, false);
 
         ImageView mapView = (ImageView)v.findViewById(R.id.MapImageView);
-        mapView.setImageResource(R.drawable.twodimensionalterrain);
+        //mapView.setImageResource(R.drawable.twodimensionalterrain);
+
+        Bitmap tiles = BitmapFactory.decodeResource(getResources(), R.drawable.twodimensionalterrain);
+        int width = tiles.getWidth();
+        Bitmap mBitmap = Bitmap.createBitmap(tiles, 0 , 0, width, width);
+        Bitmap mBitmap2 = Bitmap.createBitmap(tiles, 0 , width, width, width);
+        Bitmap bmOverlay = Bitmap.createBitmap(width * 40, width, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bmOverlay);
+
+            for(int j = 0; j < 40; j++){
+                if(j % 2 == 0)
+                    canvas.drawBitmap(mBitmap, j * width, 0, null);
+                else
+                    canvas.drawBitmap(mBitmap2, j * width, 0, null);
+            }
+
+        mapView.setImageBitmap(bmOverlay);
+
+
         return v;
     }
 }
